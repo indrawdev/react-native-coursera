474cdbae5f86c8c55b770cb009f31c9a1eafb306
indrapramana <idewebid@gmail.com>
2021-01-26 11:29:28 +0700

    Animatable

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import { ScrollView, Text, FlatList } from 'react-native';
 import { Card } from 'react-native-elements';
 import { ListItem } from 'react-native-elements';
+import * as Animatable from 'react-native-animatable';
 import { connect } from 'react-redux';
 import { LEADERS } from '../shared/leaders';
 import { baseUrl } from '../shared/baseUrl';
-
 import { Loading } from './LoadingComponent';
 
 const mapStateToProps = state => {
@@ -63,41 +63,47 @@ class About extends Component {
 				/>
 			);
 		}
-		
+
 		if (this.props.leaders.isLoading) {
 			return (
 				<ScrollView>
-					<History />
-					<Card
-						title='Corporate Leadership'>
-						<Loading />
-					</Card>
+					<Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+						<History />
+						<Card
+							title='Corporate Leadership'>
+							<Loading />
+						</Card>
+					</Animatable.View>
 				</ScrollView>
 			);
 		}
 		else if (this.props.leaders.errMess) {
 			return (
 				<ScrollView>
-					<History />
-					<Card
-						title='Corporate Leadership'>
-						<Text>{this.props.leaders.errMess}</Text>
-					</Card>
+					<Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+						<History />
+						<Card
+							title='Corporate Leadership'>
+							<Text>{this.props.leaders.errMess}</Text>
+						</Card>
+					</Animatable.View>
 				</ScrollView>
 			);
 		}
 		else {
 			return (
 				<ScrollView>
-					<History />
-					<Card
-						title='Corporate Leadership'>
-						<FlatList
-							data={this.props.leaders.leaders}
-							renderItem={renderLeader}
-							keyExtractor={item => item.id.toString()}
-						/>
-					</Card>
+					<Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+						<History />
+						<Card
+							title='Corporate Leadership'>
+							<FlatList
+								data={this.props.leaders.leaders}
+								renderItem={renderLeader}
+								keyExtractor={item => item.id.toString()}
+							/>
+						</Card>
+					</Animatable.View>
 				</ScrollView>
 			);
 		}
--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -6,6 +6,8 @@ import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { baseUrl } from '../shared/baseUrl';
 import { postFavorite, postComment } from '../redux/ActionCreators';
+import * as Animatable from 'react-native-animatable';
+
 // import { Loading } from './LoadingComponent';
 
 const mapStateToProps = state => {
@@ -27,32 +29,34 @@ function RenderDish(props) {
 
 	if (dish != null) {
 		return (
-			<Card
-				featuredTitle={dish.name}
-				image={{ uri: baseUrl + dish.image }}>
-				<Text style={{ margin: 10 }}>
-					{dish.description}
-				</Text>
-				<View style={{ flexDirection: 'row', justifyContent: 'center', }}>
-					<Icon
-						raised
-						reverse
-						name={props.favorite ? 'heart' : 'heart-o'}
-						type='font-awesome'
-						color='#f50'
-						onPress={() => props.favorite ? console.log('Already favorite') : props.onPress()}
-					/>
-					<Icon
-						raised
-						reverse
-						containerStyle={{ float: 'right' }}
-						name='pencil'
-						type='font-awesome'
-						color='#512DA8'
-						onPress={() => props.toggleCommentModal()}
-					/>
-				</View>
-			</Card>
+			<Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+				<Card
+					featuredTitle={dish.name}
+					image={{ uri: baseUrl + dish.image }}>
+					<Text style={{ margin: 10 }}>
+						{dish.description}
+					</Text>
+					<View style={{ flexDirection: 'row', justifyContent: 'center', }}>
+						<Icon
+							raised
+							reverse
+							name={props.favorite ? 'heart' : 'heart-o'}
+							type='font-awesome'
+							color='#f50'
+							onPress={() => props.favorite ? console.log('Already favorite') : props.onPress()}
+						/>
+						<Icon
+							raised
+							reverse
+							containerStyle={{ float: 'right' }}
+							name='pencil'
+							type='font-awesome'
+							color='#512DA8'
+							onPress={() => props.toggleCommentModal()}
+						/>
+					</View>
+				</Card>
+			</Animatable.View>
 		);
 	}
 	else {
@@ -81,13 +85,15 @@ function RenderComments(props) {
 	}
 
 	return (
-		<Card title="Comments">
-			<FlatList
-				data={comments}
-				renderItem={renderCommentItem}
-				keyExtractor={item => item.id.toString()}
-			/>
-		</Card>
+		<Animatable.View animation="fadeInUp" duration={2000} delay={1000}>
+			<Card title="Comments">
+				<FlatList
+					data={comments}
+					renderItem={renderCommentItem}
+					keyExtractor={item => item.id.toString()}
+				/>
+			</Card>
+		</Animatable.View>
 	);
 
 }
